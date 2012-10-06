9830a4eade7b65c752c0d7a6b7a0098361aa5b34
Oliver Chang <oliverc@cse.unsw.edu.au>
2012-10-06 21:53:19 +1000

    Some changes to allow running on CSE servers

--- app.rb
+++ app.rb
@@ -2,48 +2,50 @@ require 'sinatra'
 require 'json'
 require './timetabler'
 
-configure do 
-  set :sort_options, {'days' => 'Least days at uni',
-                      'hours' => 'Least hours at uni',
-                      'sleep_in_time' => 'Sleep in time',
-                      'start_time' => 'Latest start time',
-                      'end_time' => 'Earliest end time'}
-end
+class Octangles < Sinatra::Base 
+   configure do 
+     set :sort_options, {'days' => 'Least days at uni',
+                         'hours' => 'Least hours at uni',
+                         'sleep_in_time' => 'Sleep in time',
+                         'start_time' => 'Latest start time',
+                         'end_time' => 'Earliest end time'}
+   end
 
-helpers do
-  def get_params
-    @timetables = []
-    @input_courses = params[:courses]
-    @clash = params[:clash]
-    @sort_by = params[:sort_by]
-    @sort_by_ordered = params[:sort_by_ordered]
-    @force_courses = params[:force_courses]
-    @force_course_times = params[:force_course_times]
-    @sort_options = settings.sort_options
-  end
-end
+   helpers do
+     def get_params
+       @timetables = []
+       @input_courses = params[:courses]
+       @clash = params[:clash]
+       @sort_by = params[:sort_by]
+       @sort_by_ordered = params[:sort_by_ordered]
+       @force_courses = params[:force_courses]
+       @force_course_times = params[:force_course_times]
+       @sort_options = settings.sort_options
+     end
+   end
 
-get '/' do
-  @title = "Octangles"
-  get_params
+   get '/' do
+     @title = "Octangles"
+     get_params
 
-  erb :index
-end
+     erb :index
+   end
 
-post '/generate.json' do
-  content_type :json
-  get_params
+   post '/generate.json' do
+     content_type :json
+     get_params
 
-  course_names = @input_courses.split(',').map{|x| x.strip.upcase}.select{|x| x != '' }.uniq
-  courses = course_names.map{|x| Course.new(x)}
+     course_names = @input_courses.split(',').map{|x| x.strip.upcase}.select{|x| x != '' }.uniq
+     courses = course_names.map{|x| Course.new(x)}
 
-  force_courses = @force_courses.split(',');
-  force_course_times = @force_course_times.split(',');
+     force_courses = @force_courses.split(',');
+     force_course_times = @force_course_times.split(',');
 
-  timetables = Timetabler::generate(courses, :clash => @clash.to_i,
-                                              :sort_by => @sort_by_ordered,
-                                              :force_courses => 
-                                                force_courses.zip(force_course_times))
+     timetables = Timetabler::generate(courses, :clash => @clash.to_i,
+                                                 :sort_by => @sort_by_ordered,
+                                                 :force_courses => 
+                                                   force_courses.zip(force_course_times))
 
-  {:timetables => timetables, :courses => course_names}.to_json
+     {:timetables => timetables, :courses => course_names}.to_json
+   end
 end
