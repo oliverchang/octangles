4603ad681dd9dd0b7472b6ab064fc5177f281d20
Oliver Chang <oliver.changs@gmail.com>
2012-10-07 15:26:06 +1100

    Improve handling of non-hour starts

--- course.rb
+++ course.rb
@@ -33,7 +33,7 @@ class Course
 
   TIMETABLE_URI = 'http://www.timetable.unsw.edu.au/current/'
   TEACHING_PERIOD = 'Teaching Period One'
-  NON_HOUR_START = "Octangles currently does not take into account classes that don't start on the hour. You may get incorrect timetables."
+  NON_HOUR_START = "Octangles currently does not take into account classes that don't start on the hour. You may get suboptimal timetables as these times are rounded."
 
   def initialize(name, warnings=[])
     @name = name.upcase
@@ -68,12 +68,17 @@ class Course
           info[6].gsub(/\(.*?\)/, '').split(', ').each do |t|
             # Times are in the format "Mon 09:00 - 10:00"
             t =~ /^\s*(\w+)\s+(\d{2}):(\d{2})\s+-\s+(\d{2}):(\d{2}).*/
-            activity.add_time day_to_index($1), $2.to_i, $4.to_i
+            start = $2.to_i
+            finish = $4.to_i
 
-            if not warnings.include?(NON_HOUR_START)
+            if $3.to_i != 0 or $5.to_i != 0 
+              start -= 1 if $3.to_i != 0
+              finish += 1 if $5.to_i != 0
               # does not handle half hour starts/ends etc
-              warnings << NON_HOUR_START if $3.to_i != 0 or $5.to_i != 0
+              warnings << NON_HOUR_START if not warnings.include?(NON_HOUR_START)
             end
+
+            activity.add_time day_to_index($1), start, finish
           end
 
           activities[activity.name] ||= []
